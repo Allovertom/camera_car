c6386f3270aab66c2d637982ff21c1d6faf8f394
root <modanasianstyle20@gmail.com>
2019-10-13 22:50:09 +0900

    nd.array stack needed

--- camera_car_preprocess.py
+++ camera_car_preprocess.py
@@ -3,6 +3,20 @@ from time import sleep
 from PIL import Image
 import numpy as np
 import os
+import glob
+from sklearn import svm, metrics
+
+
+def main():
+    arr = Preprocess()
+    #print(arr, len(arr))
+#    clf = svm.SVC()
+#    clf.fit(data_train, label_train)
+#    pre = clf.predict(data_test)
+#    
+#    ac_score = metrics.accuracy_score(label_test, pre)
+#    print(ac_score)
+
 
 def Take_pics():
     camera = PiCamera()
@@ -15,9 +29,26 @@ def Take_pics():
     camera.stop_preview()
 
 def Preprocess():
-    image_P = Image.open('/home/pi/画像/image1.jpg').convert('L')
-    image_P = image_P.resize((8,8), Image.ANTIALIAS)
-    print(image_P.format, image_P.size, image_P.mode,image_P.getextrema())
-    image_P.save('/home/pi/画像/image_L.jpg')
-    
-Preprocess()
+    size = [28,28]
+    files = glob.glob('/home/pi/ドキュメント/camera_car/Train/*.jpg')
+    array = np.empty([size[0]*size[1],0],int)
+    #array = []
+    #array = np.array(array)
+    print(array.shape)
+    for file in files:
+        img = Image.open(file).convert('L')
+        img = img.resize(size, Image.ANTIALIAS)
+        print(img.format, img.size, img.mode,img.getextrema())
+        #image_P.save('/home/pi/画像/image_L0.jpg')
+        img_arr = np.asarray(img)
+        print("OD"+str(img_arr.ravel().shape))
+        array =np.hstack((array,img_arr.ravel()))
+        #array = np.append(array,img_arr.ravel(),axis=1)
+        #array = np.concatenate([array,img_arr.ravel()],axis=0)
+        print(array.shape)
+    return array.T
+
+if __name__ == '__main__':
+    main()
+
+
