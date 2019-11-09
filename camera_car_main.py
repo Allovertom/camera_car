ce9b16dc07fa26363413f3ea9cd58c1d9650dc7b
Naritomo M <you@example.com>
2019-11-09 18:00:34 +0900

    code cleansing done

--- camera_car_main.py
+++ camera_car_main.py
@@ -2,90 +2,44 @@
 from picamera import PiCamera
 import RPi.GPIO as GPIO
 from time import sleep
-from time import sleep
 from PIL import Image
 import numpy as np
 import os
 import glob
-from sklearn import svm, metrics
-from sklearn.model_selection import train_test_split
 import pickle
 import shutil
 import random
 
-PickleName = "model_20191102_9.pickle"
-honban = 0
-
-
-# MCP3208からSPI通信で12ビットのデジタル値を取得。0から7の8チャンネル使用可
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if adcnum > 7 or adcnum < 0:
-        return -1
-    GPIO.output(cspin, GPIO.HIGH)
-    GPIO.output(clockpin, GPIO.LOW)
-    GPIO.output(cspin, GPIO.LOW)
-
-    commandout = adcnum
-    commandout |= 0x18  # スタートビット＋シングルエンドビット
-    commandout <<= 3    # LSBから8ビット目を送信するようにする
-    for i in range(5):
-        # LSBから数えて8ビット目から4ビット目までを送信
-        if commandout & 0x80:
-            GPIO.output(mosipin, GPIO.HIGH)
-        else:
-            GPIO.output(mosipin, GPIO.LOW)
-        commandout <<= 1
-        GPIO.output(clockpin, GPIO.HIGH)
-        GPIO.output(clockpin, GPIO.LOW)
-    adcout = 0
-    # 13ビット読む（ヌルビット＋12ビットデータ）
-    for i in range(13):
-        GPIO.output(clockpin, GPIO.HIGH)
-        GPIO.output(clockpin, GPIO.LOW)
-        adcout <<= 1
-        if i>0 and GPIO.input(misopin)==GPIO.HIGH:
-            adcout |= 0x1
-    GPIO.output(cspin, GPIO.HIGH)
-    return adcout
-    
-def Preprocess(i,honban):
+def Preprocess(i,SaveP):
     size = [28,28]
     array = np.empty([size[0]*size[1],0],int)
     print(array.shape)
     FullPath = glob.glob('/home/pi/ドキュメント/camera_car/Predict/*.jpg')
     print(FullPath)
-    #前処理
+    #Preprocessing
     img = Image.open(FullPath[0]).convert('L')
     img = img.resize(size, Image.ANTIALIAS)
     print(img.format, img.size, img.mode,img.getextrema())
-    #一次元化
+    #Make one dimention array
     img_arr = np.asarray(img)
-    print("OD"+str(img_arr.ravel().shape))
-    if honban:
-        os.remove(FullPath[0])#本番用
-    else:
+    print("OneDimention"+str(img_arr.ravel().shape))
+    if SaveP:
+        os.remove(FullPath[0])
+    else:#saving pics for re-training the model.才が羽州のために写真保存
         shutil.move(FullPath[0],'/home/pi/ドキュメント/camera_car/Predict/done/%s.jpg' % i)
     return img_arr.ravel(), img
 
-GPIO.setmode(GPIO.BCM)
-# ピンの名前を変数として定義
-#SPICLK = 11
-#SPIMOSI = 10
-#SPIMISO = 9
-#SPICS = 8
-# SPI通信用の入出力を定義
-#GPIO.setup(SPICLK, GPIO.OUT)
-#GPIO.setup(SPIMOSI, GPIO.OUT)
-#GPIO.setup(SPIMISO, GPIO.IN)
-#GPIO.setup(SPICS, GPIO.OUT)
 
+PickleName = "model_20191102_9.pickle"#indicate trained model(pickle)
+SavePics = 0
 with open(PickleName, mode='rb') as fp:
     clf = pickle.load(fp)
 
 camera = PiCamera()
-#camera.rotation = 180
+#camera.rotation = 180#if camera is upside down カメラが上下反対の場合
 camera.resolution = (64,64)
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
@@ -102,27 +56,25 @@ p2.start(0)
 p3.start(0)
 print("start moving...")
 sleep(10)
-#adc_pin0 = 0
 i = 0
 duty = 70
-#まずは前進
+
+
+#At first go forward
 p0.ChangeDutyCycle(20)
 p1.ChangeDutyCycle(0)
 p2.ChangeDutyCycle(20)
 p3.ChangeDutyCycle(0)
-#sleep(3)
 try:
     while True:
-#        inputVal0 = readadc(adc_pin0, SPICLK, SPIMOSI, SPIMISO, SPICS)
-
-        #写真取って指定フォルダに保存
+        #Take a pic and save to indicated folder.写真取って指定フォルダに保存
         i += 1
         camera.capture('/home/pi/ドキュメント/camera_car/Predict/%s.jpg' % i)
-        #指定フォルダの写真を前処理
-        X_pred, img = Preprocess(i,honban)
-        #推定
+        #preprocessing at indicated folder.指定フォルダの写真を前処理
+        X_pred, img = Preprocess(i, SavePics)
+        #predict 推定
         pred = clf.predict(X_pred)
-        #デューティー比変更
+        #change duty ratio.デューティー比変更
         if pred[0] == 0:#前進
             print("Forward")
             p0.ChangeDutyCycle(duty)
@@ -130,21 +82,21 @@ try:
             p2.ChangeDutyCycle(duty)
             p3.ChangeDutyCycle(0)
             sleep(0.8)
-        elif pred[0] == 1:#左折
+        elif pred[0] == 1:#left 左折
             print("Left")
             p0.ChangeDutyCycle(duty-20)
             p1.ChangeDutyCycle(0)
             p2.ChangeDutyCycle(0)
             p3.ChangeDutyCycle(20)
             sleep(0.3)
-        elif pred[0] == 2:#右折
+        elif pred[0] == 2:#right 右折
             print("Right")
             p0.ChangeDutyCycle(0)
             p1.ChangeDutyCycle(20)
             p2.ChangeDutyCycle(duty-20)
             p3.ChangeDutyCycle(0)
             sleep(0.3)
-        elif pred[0]  == 3:#後退
+        elif pred[0]  == 3:#backword 後退
             p0.ChangeDutyCycle(0)
             p1.ChangeDutyCycle(duty-10)
             p2.ChangeDutyCycle(0)
@@ -152,10 +104,10 @@ try:
             print("Backing...")
             sleep(1)
             print("finish backing")
-        #前処理後写真を保存
-        if honban:
+        #save preprocessed pic前処理後写真を保存
+        if SavePics:
             pass
-        else:
+        else:#collect preprocessed pics with pred number.再学習用写真（推定値付き）保存
             rand = random.randint(0,100000)
             img.save('/home/pi/ドキュメント/camera_car/Train/'
                      +str(i)+'_'+str(int(pred[0]))+
--- camera_car_preprocess.py
+++ camera_car_preprocess.py
@@ -1,18 +1,15 @@
 #from picamera import PiCamera
-from time import sleep
 from PIL import Image
 import numpy as np
-import os
 import glob
 from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split
 import pickle
 
-ModelName = "model_20191102_9.pickle"
+ModelName = "model_20191102_9.pickle"#As you wish.
 
 def main():
     Path = '/home/pi/ドキュメント/camera_car/Train/'
-    #Path = 'Train/'
     Left_L = glob.glob(Path + '1_Left/*.jpg')
     Right_L = glob.glob(Path + '2_Right/*.jpg')
     Center_L = glob.glob(Path + '0_Center/*.jpg')
@@ -26,9 +23,9 @@ def main():
     X_B = Preprocess(Back_L)
     Y_B = np.full(int(len(X_B)/784),3)
     
-    X = np.r_[X_L, X_R, X_C, X_B]
-    X = X.reshape([int(len(X)/784),784])
-    y = np.r_[Y_L, Y_R, Y_C, Y_B]
+    X = np.r_[X_L, X_R, X_C, X_B]#concatinate all preprocessed pics.全前処理写真結合
+    X = X.reshape([int(len(X)/784),784])#make array.行列化
+    y = np.r_[Y_L, Y_R, Y_C, Y_B]#teacher data addition.教師データ付加
     print(X.shape)
     print(y.shape)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
@@ -37,23 +34,11 @@ def main():
     clf.fit(X_train, y_train)
     pre = clf.predict(X_test)    
     ac_score = metrics.accuracy_score(y_test, pre)
-    print(ac_score)
+    print(ac_score)#print score.精度表示
 
-    
-        
     with open(ModelName, mode='wb') as fp:
-        pickle.dump(clf, fp)
+        pickle.dump(clf, fp)#save model.モデル出力
 
-
-def Take_pics():
-    camera = PiCamera()
-    camera.rotation = 180
-    camera.resolution = (64,64)
-    camera.start_preview()
-    for i in range(5):
-        sleep(2)
-        camera.capture('/home/pi/画像/image%s.jpg' % i)
-    camera.stop_preview()
 
 def Preprocess(files):
     size = [28,28]
@@ -64,8 +49,6 @@ def Preprocess(files):
         img = Image.open(file).convert('L')
         img = img.resize(size, Image.ANTIALIAS)
         print(img.format, img.size, img.mode,img.getextrema())
-        #img.save('/home/pi/ドキュメント/camera_car/Prep/'+str(i)+'_out.jpg')
-        #img.save('Prep/'+str(i)+'_out.jpg')
         img_arr = np.asarray(img)
         print("OD"+str(img_arr.ravel().shape))
         array = np.append(array,img_arr.ravel())
